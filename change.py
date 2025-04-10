0e2a8d0b59c9b7759ad541a6ee7c347207cb52c3
SantinoAlonso25 <salonso1@mail.austral.edu.ar>
2025-04-10 10:01:06 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,1 @@
-expense = 23.75
-money = 100
-vuelto = money - expense
-pesos = int(vuelto)
-centavos = vuelto - pesos
-resultado_centavos = int(centavos*100)
-print (f"Ingresar gasto:")
-print (expense)
-print (f"Dinero recibido")
-print (money)
-print ("")
-print ("Vuelto")
-print ("")
-print (f"Pesos:")
-print (pesos)
-print (f"Centavos:")
-print (resultado_centavos)
+
